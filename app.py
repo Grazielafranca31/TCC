1091480633df6a40f4d5337cbd7dfc865db326f8
Graziela França <38873168+Grazielafranca31@users.noreply.github.com>
2023-04-03 09:52:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask
 import requests
 import pandas as pd
 import json
-!pip install datetime
 from datetime import date
 from datetime import datetime
 import sendgrid
@@ -15,7 +14,6 @@ import requests
 app = Flask(__name__)
 
 @app.route("/")
-
 def gastos_deputados():
   return "olá"
 
