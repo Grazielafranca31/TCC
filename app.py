1f68f32a0336d4761de9e5a8532e30e5abb8671a
Graziela França <38873168+Grazielafranca31@users.noreply.github.com>
2023-04-02 22:01:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 from flask import Flask
+
+import requests
+import pandas as pd
+import json
+!pip install datetime
+from datetime import date
+from datetime import datetime
+import sendgrid
+import os
+from sendgrid.helpers.mail import Mail, Email, To, Content
+import getpass
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
